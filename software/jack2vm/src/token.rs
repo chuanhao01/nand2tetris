a212f587c6731d412e074450561b8f51cce995db
chuanhao01 <34702921+chuanhao01@users.noreply.github.com>
2024-11-02 00:49:31 +0800

    [feat] get_source string
    Description
    removes "" when using string

--- software/jack2vm/src/token.rs
+++ software/jack2vm/src/token.rs
@@ -1,3 +1,5 @@
+use std::fmt::Display;
+
 #[derive(Debug, Clone)]
 pub struct Token {
     pub _type: TokenType,
@@ -14,10 +16,17 @@ impl Token {
             line,
         }
     }
-    pub fn get_source(&mut self, source: &Vec<char>) -> String {
-        source[self.start..self.start + self.length]
-            .iter()
-            .collect()
+    pub fn get_source(&self, source: &[char]) -> String {
+        match self._type {
+            // Ignore the ""
+            TokenType::String => source[self.start + 1..self.start + self.length - 1]
+                .iter()
+                .collect(),
+
+            _ => source[self.start..self.start + self.length]
+                .iter()
+                .collect(),
+        }
     }
 }
 
@@ -78,4 +87,49 @@ pub enum Symbols {
     Asterisk,     // *
     Slash,        // /
     Dot,          // .
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn get_source_not_string() {
+        let source = String::new().chars().collect::<Vec<char>>();
+        let token = Token {
+            _type: TokenType::EOF,
+            start: 0,
+            length: 0,
+            line: 1,
+        };
+        assert_eq!(token.get_source(&source), String::new());
+
+        let source = "!".chars().collect::<Vec<char>>();
+        let token = Token {
+            _type: TokenType::Symbol(Symbols::Bang),
+            start: 0,
+            length: 1,
+            line: 1,
+        };
+        assert_eq!(token.get_source(&source), "!".to_string());
+
+        let source = "class".chars().collect::<Vec<char>>();
+        let token = Token {
+            _type: TokenType::Keyword(ReservedKeywords::Class),
+            start: 0,
+            length: 5,
+            line: 1,
+        };
+        assert_eq!(token.get_source(&source), "class".to_string());
+    }
+    #[test]
+    fn get_source_string() {
+        let source = "\"funny string\"".chars().collect::<Vec<char>>();
+        let token = Token {
+            _type: TokenType::String,
+            start: 0,
+            length: 14,
+            line: 1,
+        };
+        assert_eq!(token.get_source(&source), "funny string".to_string());
+    }
 }
