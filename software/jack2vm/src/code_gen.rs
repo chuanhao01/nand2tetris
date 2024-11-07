8255d9be0b182ba72d6da32147a55756513a68c0
chuanhao01 <34702921+chuanhao01@users.noreply.github.com>
2024-11-08 01:02:50 +0800

    [WIP] symbols

--- software/jack2vm/src/code_gen.rs
+++ software/jack2vm/src/code_gen.rs
@@ -76,8 +76,101 @@ impl CodeGen {
                 ))
             }
         };
-        // self.class_symbol_table.insert(name, VariableMetaData{_type, kind, })
-        Ok(())
+        let number = match variable_kind {
+            VariableKind::Static => {
+                self.static_counter += 1;
+                self.static_counter - 1
+            }
+            VariableKind::Field => {
+                self.field_counter += 1;
+                self.field_counter - 1
+            }
+            _ => panic!("Should not fail insert class variable"),
+        };
+        match self.class_symbol_table.insert(
+            name.clone(),
+            VariableMetaData {
+                _type: variable_type,
+                kind: variable_kind,
+                number,
+            },
+        ) {
+            Some(_) => Err(format!(
+                "Class variable {} is defined again on line {}",
+                name, kind.line
+            )),
+            None => Ok(()),
+        }
+    }
+    fn reset_subroutine_table(&mut self, class_name: String) {
+        self.subroutine_symbol_table = HashMap::default();
+        self.local_counter = 0;
+        self.argument_counter = 0;
+    }
+    fn insert_subroutine_variable(
+        &mut self,
+        name: String,
+        kind: Token,
+        _type: Token,
+        source: &[char],
+    ) -> ParserReturn {
+        let variable_kind = match &kind._type {
+            TokenType::Keyword(ReservedKeywords::Static) => VariableKind::Static,
+            TokenType::Keyword(ReservedKeywords::Field) => VariableKind::Field,
+            _ => {
+                return Err(Parser::error_expected_token_type(
+                    &kind,
+                    &[
+                        TokenType::Keyword(ReservedKeywords::Static),
+                        TokenType::Keyword(ReservedKeywords::Field),
+                    ],
+                    source,
+                ))
+            }
+        };
+        let variable_type = match &_type._type {
+            TokenType::Keyword(ReservedKeywords::Int) => VariableType::Int,
+            TokenType::Keyword(ReservedKeywords::Char) => VariableType::Char,
+            TokenType::Keyword(ReservedKeywords::Boolean) => VariableType::Boolean,
+            TokenType::Identifier => VariableType::Identifier(_type.get_source(source)),
+            _ => {
+                return Err(Parser::error_expected_token_type(
+                    &_type,
+                    &[
+                        TokenType::Keyword(ReservedKeywords::Int),
+                        TokenType::Keyword(ReservedKeywords::Char),
+                        TokenType::Keyword(ReservedKeywords::Boolean),
+                        TokenType::Identifier,
+                    ],
+                    source,
+                ))
+            }
+        };
+        let number = match variable_kind {
+            VariableKind::Static => {
+                self.static_counter += 1;
+                self.static_counter - 1
+            }
+            VariableKind::Field => {
+                self.field_counter += 1;
+                self.field_counter - 1
+            }
+            _ => panic!("Should not fail insert class variable"),
+        };
+        match self.class_symbol_table.insert(
+            name.clone(),
+            VariableMetaData {
+                _type: variable_type,
+                kind: variable_kind,
+                number,
+            },
+        ) {
+            Some(_) => Err(format!(
+                "Class variable {} is defined again on line {}",
+                name, kind.line
+            )),
+            None => Ok(()),
+        }
     }
 }
 impl Default for CodeGen {
